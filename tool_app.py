f69f8939af3890b8123b10bbe32b7e2d6174c8a5
Maria Czapczyk (FläktGroup) <maria.czapczyk@flaktwoods.net>
2022-08-22 13:57:31 +0200

    Add conversion Time(s) to format hh:mm:ss

--- tool_app.py
+++ tool_app.py
@@ -1,5 +1,7 @@
+import datetime
+from tkinter import N
 from typing import Any
-from venv import create
+from xmlrpc.client import DateTime
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -24,9 +26,14 @@ if uploaded_file is not None:
     selected_sheet = st.radio('Choose: ', sheet_list)
     
     def create_df():
-        return raw_data.parse(selected_sheet)
+        df = raw_data.parse(selected_sheet)
+        #convert time in s to time in format hh:mm:ss
+        for _ in df['Time (s)']:
+            df['Time (s)'][_] = str(datetime.timedelta(seconds=_))
+        return df
+    
+    df = create_df()
 
-df = create_df()
 st.markdown("""---""")
 
 #Section: Select columns
@@ -34,24 +41,25 @@ st.subheader('Select columns')
 
 all_columns = st.checkbox('Select all columns')
 if all_columns:
-    columns = df.columns
+    columns = df.columns[1:]
     st.table(columns)
 else:
-    columns = st.multiselect('Select columns (start from Time):', df.columns, default=None)
+    columns = st.multiselect('Select columns:', df.columns, default=None)
     confirm_btn = st.button('Apply')
     if confirm_btn == True:
         st.table(columns)
 st.markdown("""---""")
 
 #Section: Plot graph for selected columns
-st.subheader('Graph data')
 graph_data = pd.DataFrame(df[columns])
 
 st.subheader("Plot graph")
-title = st.text_input("Plot name")
-st.subheader(title)
+title = st.text_input("Graph title")
+plot_btn = st.button('Plot!')
 
-
+if (title is not '') & (plot_btn == True):
+    plot = px.line(graph_data, title= title, hover_data=[])
+    st.write(plot)
 
 
 #Show/hide raw data frame
